1b1a4ef8b2a89f771a6fc283d32bd471244c2db7
Dominic Benintendi <bdominic00@gmail.com>
2023-10-31 16:53:14 -0400

    Added server and two clientsv2

--- src/client1.py
+++ src/client1.py
@@ -3,7 +3,7 @@ import json
 
 def send_message(message):
     data = {'message': message}
-    response = requests.post('http://localhost:5000/send_message', json=data)
+    response = requests.post('http://192.168.1.111:5000/send_message', json=data)
     return response.json()
 
 if __name__ == '__main__':
